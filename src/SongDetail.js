10438d010e8d86147b01802e8bad8b1b1b15601a
Alfredo Arias <alfredo.arias@truextend.com>
2018-03-24 09:48:14 -0400

    play track functionallity

--- src/SongDetail.js
+++ src/SongDetail.js
@@ -4,28 +4,52 @@ import {
   View,
   Text
 } from 'react-native';
-import {Button} from 'react-native-elements'
+import Sound from 'react-native-sound';
+import { Button, Card, Icon } from 'react-native-elements';
 
 type Props = {};
 export default class SongDetail extends Component<Props> {
 
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      song: {}
+    }
+
+    this.state.song = this.props.navigation.state.params;
   }
 
   componentDidMount() {
 
   }
 
-  render() {
-    const song = this.props.navigation.state.params;
+  playTrack = () => {
+    const track = new Sound(this.state.song.previewUrl, null, (e) => {
+      if (e) {
+        console.log('error loading track:', e)
+      } else {
+        track.play()
+      }
+    })
+  }
 
+  render() {
     return (
       <View>
-        <Text>
-          hello detail {song.trackName}
-        </Text>
+
+        <Card
+          title={this.state.song.artistName}
+          image={{uri: this.state.song.artworkUrl100}} >
+          <Text style={{marginBottom: 10}}>
+            {this.state.song.trackName}
+          </Text>
+          <Icon
+            raised
+            name='play'
+            type='font-awesome'
+            color='#f50'
+            onPress={() => this.playTrack()} />
+        </Card>
       </View>
     );
   }
